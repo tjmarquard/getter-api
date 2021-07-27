ee9cbfe75fac4ca99c02f0283f7709ace2cf2180
Tim <tjmarquard@gmail.com>
2021-07-26 21:16:29 -0500

    Query controller setup

---
+++ GetterApi/Controllers/QueryController.cs
@@ -0,0 +1,27 @@
+﻿using GetterApi.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QueryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{domain}")]
+        public async Task<IActionResult> Get(string domain, [FromQuery] string [] services)
+        {
+            if (!Enum.IsDefined(typeof(Domains), domain.ToUpperInvariant()))
+            {
+                return BadRequest();
+            }
+
+            var rng = new Random();
+            return Ok(Enumerable.Range(1, 5).Select(index => rng.Next(1, 10)).ToArray());
+        }
+    }
+}
