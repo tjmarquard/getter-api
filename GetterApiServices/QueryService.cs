18de66db2c5c84fede63e3ce7ae639d898f69205
Tim <tjmarquard@gmail.com>
2021-07-28 16:28:46 -0500

    fixed dependency issues

---
+++ GetterApiServices/QueryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GetterApiServices
+{
+    public interface IQueryService
+    {
+        string GetWeatherApiEndPoints(string[] services);
+    }
+
+    public class QueryService : IQueryService
+    {
+        public string GetWeatherApiEndPoints(string[] services)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
